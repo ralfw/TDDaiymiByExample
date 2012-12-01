c2755a1df7ac78bbc7c10cbaf9bbdb225a329971
Ralf Westphal <info@ralfw.de>
2012-12-01 17:51:44 +0100

    Green: Multiple name value pairs

--- Examples/CSharp/ToDictionary/ToDictionary/test_ToDictionary.cs
+++ Examples/CSharp/ToDictionary/ToDictionary/test_ToDictionary.cs
@@ -17,5 +17,21 @@ namespace ToDictionary
             var result = new Dictionary<string, string> {{name, value}};
             Assert.That(result, Is.EqualTo(new Dictionary<string,string>{{"a", "1"}}));
         }
+
+        [Test]
+        public void Multiple_name_value_pairs()
+        {
+            var nvpairs = "a=1;b=2".Split(';');
+
+            var name = nvpairs[0].Substring(0, 1);
+            var value = nvpairs[0].Substring(2, 1);
+            var result = new Dictionary<string, string> {{name, value}};
+
+            name = nvpairs[1].Substring(0, 1);
+            value = nvpairs[1].Substring(2, 1);
+            result.Add(name,value);
+
+            Assert.That(result, Is.EqualTo(new Dictionary<string,string>{{"a", "1"},{"b", "2"}}));
+        }
     }
 }
