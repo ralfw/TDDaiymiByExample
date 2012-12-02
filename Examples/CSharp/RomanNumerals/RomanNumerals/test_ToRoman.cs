e0270ec8a5bfe4eb6d3f685ffd7edb2b46fec1cf
Ralf Westphal <info@ralfw.de>
2012-12-02 11:44:08 +0100

    Green: Acceptance test
    Added missing building blocks

--- Examples/CSharp/RomanNumerals/RomanNumerals/test_ToRoman.cs
+++ Examples/CSharp/RomanNumerals/RomanNumerals/test_ToRoman.cs
@@ -10,9 +10,17 @@ namespace RomanNumerals
     public class test_ToRoman
     {
         private readonly List<KeyValuePair<int,string>> _romanNumerals = new List<KeyValuePair<int,string>> { 
+                                                                                new KeyValuePair<int, string>(1000, "M"),
                                                                                 new KeyValuePair<int, string>(900, "CM"),
+                                                                                new KeyValuePair<int, string>(500, "D"),
+                                                                                new KeyValuePair<int, string>(400, "CD"),
+                                                                                new KeyValuePair<int, string>(100, "C"),
+                                                                                new KeyValuePair<int, string>(90, "XC"),
+                                                                                new KeyValuePair<int, string>(50, "L"),
+                                                                                new KeyValuePair<int, string>(40, "XL"),
                                                                                 new KeyValuePair<int, string>(9, "IX"),
                                                                                 new KeyValuePair<int, string>(5, "V"), 
+                                                                                new KeyValuePair<int, string>(4, "IV"),
                                                                                 new KeyValuePair<int, string>(1, "I")};
 
         [TestCase(5, Result = "V")]
