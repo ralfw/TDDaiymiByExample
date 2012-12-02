ffbbdfacd811246f6564e2af921b5fa7e1b33522
Ralf Westphal <info@ralfw.de>
2012-12-02 13:06:01 +0100

    Enqueue into empty queue

--- Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
+++ Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -8,5 +9,19 @@ namespace Systen.Collections.Generic
     [TestFixture]
     public class test_PriorityQueue
     {
+        [Test]
+        public void Enqueue_into_empty_queue()
+        {
+            var queue = new List<Element>();
+            queue.Add(new Element("a", 10));
+            Assert.That(queue, Is.EqualTo(new[]{new Element("a", 10)}));
+        }
+
+        struct Element
+        {
+            public object Value;
+            public int Priority;
+            public Element(object value, int priority) { Value = value; Priority = priority; }
+        }
     }
 }
