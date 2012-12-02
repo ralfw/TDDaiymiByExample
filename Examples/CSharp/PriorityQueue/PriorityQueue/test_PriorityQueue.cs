e785dc881d1c05a5ca1f4dcd9a5b2ea5ec412155
Ralf Westphal <info@ralfw.de>
2012-12-02 17:05:44 +0100

    Red: Enqueue in no order
    Neither the reverse order technique works (Insert), nor the same
    priority technique (Add). Something more has to be done.

--- Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
+++ Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
@@ -26,12 +26,21 @@ namespace Systen.Collections.Generic
         }
 
         [Test]
-        public void Enqueue_with_different_priority_but_in_reverse_order()
+        public void Enqueue_with_different_priorities_in_reverse_order()
         {
             _queue.Insert(0, new Element("c", 1));
             _queue.Insert(0, new Element("b", 5));
             _queue.Insert(0, new Element("a", 10));
             Assert.That(_queue, Is.EqualTo(new[] { new Element("a", 10), new Element("b", 5), new Element("c", 1) }));    
+        }
+
+        [Test]
+        public void Enqueue_with_different_priorities_in_no_order()
+        {
+            _queue.Insert(0, new Element("b", 5));
+            _queue.Insert(0, new Element("c", 1));
+            _queue.Insert(0, new Element("a", 10));
+            Assert.That(_queue, Is.EqualTo(new[] { new Element("a", 10), new Element("b", 5), new Element("c", 1) }));  
         }
 
 
