f75c9e570055a88af3e7515d58224124815ef2db
Ralf Westphal <info@ralfw.de>
2012-12-02 11:24:13 +0100

    Switched from dictionary to list
    Since the decimal for the next building block is not known, it needs to
    be searched. That´s not what dictionaries are for.

--- Examples/CSharp/RomanNumerals/RomanNumerals/test_ToRoman.cs
+++ Examples/CSharp/RomanNumerals/RomanNumerals/test_ToRoman.cs
@@ -9,20 +9,22 @@ namespace RomanNumerals
     [TestFixture]
     public class test_ToRoman
     {
-        private readonly Dictionary<int,string> _romanNumerals = new Dictionary<int, string> {{5, "V"}, {1, "I"}};
+        private readonly List<KeyValuePair<int,string>> _romanNumerals = new List<KeyValuePair<int,string>> { 
+                                                                                new KeyValuePair<int, string>(5, "V"), 
+                                                                                new KeyValuePair<int, string>(1, "I")};
 
         [Test]
         public void Decimal_matches_building_block()
         {
-            var roman = "" + _romanNumerals[5];
+            var roman = "" + _romanNumerals.First(kvp => kvp.Key == 5).Value;
             Assert.AreEqual("V", roman);
         }
 
         [Test]
         public void Decimal_needs_to_be_build_from_non_repeating_building_blocks()
         {
-            var roman = "" + _romanNumerals[5];
-            roman = roman + _romanNumerals[1];
+            var roman = "" + _romanNumerals.First(kvp => kvp.Key == 5).Value;
+            roman = roman + _romanNumerals.First(kvp => kvp.Key == 1).Value;
             Assert.AreEqual("VI", roman);
         }
 
@@ -33,15 +35,15 @@ namespace RomanNumerals
 
             var buildingBlockKey = 5;
             decimalNumber -= buildingBlockKey;
-            var roman = "" + _romanNumerals[buildingBlockKey];
+            var roman = "" + _romanNumerals.First(kvp => kvp.Key == buildingBlockKey).Value;
 
             buildingBlockKey = 1;
             decimalNumber -= buildingBlockKey;
-            roman = roman + _romanNumerals[buildingBlockKey];
+            roman = roman + _romanNumerals.First(kvp => kvp.Key == buildingBlockKey).Value;
 
             buildingBlockKey = 1;
             decimalNumber -= buildingBlockKey;
-            roman = roman + _romanNumerals[1];
+            roman = roman + _romanNumerals.First(kvp => kvp.Key == buildingBlockKey).Value;
 
             Assert.AreEqual("VII", roman);
             Assert.AreEqual(0, decimalNumber);
