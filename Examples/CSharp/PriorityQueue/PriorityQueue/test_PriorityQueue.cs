188184f1f9d30b6fb1723a5a1196fc170f7f9b7b
Ralf Westphal <info@ralfw.de>
2012-12-02 13:09:12 +0100

    Enqueue with same priority

--- Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
+++ Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
@@ -17,6 +17,17 @@ namespace Systen.Collections.Generic
             Assert.That(queue, Is.EqualTo(new[]{new Element("a", 10)}));
         }
 
+        [Test]
+        public void Enqueue_with_same_prio()
+        {
+            var queue = new List<Element>();
+            queue.Add(new Element("a", 5));
+            queue.Add(new Element("b", 5));
+            queue.Add(new Element("c", 5));
+            Assert.That(queue, Is.EqualTo(new[] { new Element("a", 5), new Element("b", 5), new Element("c", 5) }));            
+        }
+
+
         struct Element
         {
             public object Value;
