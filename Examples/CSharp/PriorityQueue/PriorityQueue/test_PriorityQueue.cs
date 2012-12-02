0a6daa7da2b717b47a398fc2a857972db5883a8d
Ralf Westphal <info@ralfw.de>
2012-12-02 17:39:53 +0100

    Moved code into Peek() method of new PriorityQueue class
    That was a pretty big refactoring. But it seemed a good time to set up
    the class for both the Peek method as well as Enqueue().

--- Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
+++ Examples/CSharp/PriorityQueue/PriorityQueue/test_PriorityQueue.cs
@@ -13,35 +13,43 @@ namespace Systen.Collections.Generic
         [Test]
         public void Enqueue_into_empty_queue()
         {
-            Enqueue("a", 10);
-            Assert.That(_queue, Is.EqualTo(new[]{new Element("a", 10)}));
+            var queue = new List<PriorityQueue<string>.Element>();
+            var sut = new PriorityQueue<string>(queue);
+            sut.Enqueue("a", 10);
+            Assert.That(queue, Is.EqualTo(new[]{new PriorityQueue<string>.Element("a", 10)}));
         }
 
         [Test]
         public void Enqueue_with_same_prio()
         {
-            Enqueue("a", 5);
-            Enqueue("b", 5);
-            Enqueue("c", 5);
-            Assert.That(_queue, Is.EqualTo(new[] { new Element("a", 5), new Element("b", 5), new Element("c", 5) }));            
+            var queue = new List<PriorityQueue<string>.Element>();
+            var sut = new PriorityQueue<string>(queue);
+            sut.Enqueue("a", 5);
+            sut.Enqueue("b", 5);
+            sut.Enqueue("c", 5);
+            Assert.That(queue, Is.EqualTo(new[] { new PriorityQueue<string>.Element("a", 5), new PriorityQueue<string>.Element("b", 5), new PriorityQueue<string>.Element("c", 5) }));            
         }
 
         [Test]
         public void Enqueue_with_different_priorities_in_reverse_order()
         {
-            Enqueue("c", 1);
-            Enqueue("b", 5);
-            Enqueue("a", 10);
-            Assert.That(_queue, Is.EqualTo(new[] { new Element("a", 10), new Element("b", 5), new Element("c", 1) }));    
+            var queue = new List<PriorityQueue<string>.Element>();
+            var sut = new PriorityQueue<string>(queue);
+            sut.Enqueue("c", 1);
+            sut.Enqueue("b", 5);
+            sut.Enqueue("a", 10);
+            Assert.That(queue, Is.EqualTo(new[] { new PriorityQueue<string>.Element("a", 10), new PriorityQueue<string>.Element("b", 5), new PriorityQueue<string>.Element("c", 1) }));    
         }
 
         [Test]
         public void Enqueue_with_different_priorities_in_no_order()
         {
-            Enqueue("b", 5);
-            Enqueue("c", 1);
-            Enqueue("a", 10);
-            Assert.That(_queue, Is.EqualTo(new[] { new Element("a", 10), new Element("b", 5), new Element("c", 1) })); 
+            var queue = new List<PriorityQueue<string>.Element>();
+            var sut = new PriorityQueue<string>(queue);
+            sut.Enqueue("b", 5);
+            sut.Enqueue("c", 1);
+            sut.Enqueue("a", 10);
+            Assert.That(queue, Is.EqualTo(new[] { new PriorityQueue<string>.Element("a", 10), new PriorityQueue<string>.Element("b", 5), new PriorityQueue<string>.Element("c", 1) })); 
         }
         #endregion
 
@@ -49,14 +57,16 @@ namespace Systen.Collections.Generic
         [Test]
         public void Peek_on_empty()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => { var _ = _queue[0].Value; });
+            var sut = new PriorityQueue<string>();
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.Peek());
         }
 
         [Test]
         public void Peek_on_non_empty()
         {
-            Enqueue("a", 1);
-            Assert.AreEqual("a", _queue[0].Value);
+            var sut = new PriorityQueue<string>();
+            sut.Enqueue("a", 1);
+            Assert.AreEqual("a", sut.Peek());
         }
         #endregion
 
@@ -68,26 +78,26 @@ namespace Systen.Collections.Generic
 
         #region Clear
         #endregion
+    }
 
 
-        [SetUp]
-        public void Initialize()
+    public class PriorityQueue<T>
+    {
+        internal struct Element
         {
-            _queue = new List<Element>();
+            public readonly T Value;
+            public readonly int Priority;
+            public Element(T value, int priority) { Value = value; Priority = priority; }
         }
 
 
-        struct Element
-        {
-            public object Value;
-            public int Priority;
-            public Element(object value, int priority) { Value = value; Priority = priority; }
-        }
+        private readonly List<Element> _queue;
 
 
-        private List<Element> _queue;
+        internal PriorityQueue(List<Element> queue) { _queue = queue; }
+        public PriorityQueue() : this(new List<Element>()) {} 
 
-        internal void Enqueue<T>(T value, int priority)
+        public void Enqueue(T value, int priority)
         {
             var indexOfLowerPrioElement = _queue.Where(e => e.Priority < priority)
                                                 .Select((e, i) => i)
@@ -97,6 +107,11 @@ namespace Systen.Collections.Generic
                 _queue.Insert(indexOfLowerPrioElement.First(), new Element(value, priority));
             else
                 _queue.Add(new Element(value, priority));
+        }
+
+        public T Peek()
+        {
+            return _queue[0].Value;
         }
     }
 }
