06bef18e8cb8ca4af03965a69d6c787087289882
Ralf Westphal <info@ralfw.de>
2012-12-09 15:19:40 +0100

    Green: First win
    Both scores show up as vars; but the winning player is only implicitly
    visible through the higher player1 score.

--- Examples/CSharp/Tennis/Tennis/test_RegisterWinFor.cs
+++ Examples/CSharp/Tennis/Tennis/test_RegisterWinFor.cs
@@ -12,6 +12,9 @@ namespace Tennis
         [Test]
         public void First_win()
         {
+            var scorePlayer1 = 15;
+            var scorePlayer2 = 0;
+            var score = string.Format("{0}:{1}", scorePlayer1, scorePlayer2);
             Assert.AreEqual("15:0", score);
         }
     }
