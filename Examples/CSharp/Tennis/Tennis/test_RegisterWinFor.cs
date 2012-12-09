a8ddc536c77952fe7efefecf97f5a116c4436644
Ralf Westphal <info@ralfw.de>
2012-12-09 15:26:24 +0100

    Second win same player
    Changed strategy to count score: switched to index into point array

--- Examples/CSharp/Tennis/Tennis/test_RegisterWinFor.cs
+++ Examples/CSharp/Tennis/Tennis/test_RegisterWinFor.cs
@@ -17,5 +17,19 @@ namespace Tennis
             var score = string.Format("{0}:{1}", scorePlayer1, scorePlayer2);
             Assert.AreEqual("15:0", score);
         }
+
+        [Test]
+        public void Second_win_same_player()
+        {
+            var pointValues = new[] {"0", "15", "30"};
+            var pointIndexPlayer1 = 0;
+            var pointindexPlayer2 = 0;
+
+            pointIndexPlayer1++;
+            pointIndexPlayer1++;
+            
+            var score = string.Format("{0}:{1}", pointValues[pointIndexPlayer1], pointValues[pointindexPlayer2]);
+            Assert.AreEqual("30:0", score);
+        }
     }
 }
