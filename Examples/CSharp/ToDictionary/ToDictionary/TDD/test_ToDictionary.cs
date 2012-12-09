638a4aecb5675ec93307e44692e2b2b0846774d9
Ralf Westphal <info@ralfw.de>
2012-12-09 09:43:36 +0100

    Red: Equal in value

--- Examples/CSharp/ToDictionary/ToDictionary/TDD/test_ToDictionary.cs
+++ Examples/CSharp/ToDictionary/ToDictionary/TDD/test_ToDictionary.cs
@@ -50,5 +50,11 @@ namespace ToDictionary.TDD
         {
             Assert.That("a= 1 ".ToDictionary(), Is.EqualTo(new Dictionary<string, string> { { "a", " 1 " } })); 
         }
+
+        [Test]
+        public void Equal_in_value()
+        {
+            Assert.That("a==".ToDictionary(), Is.EqualTo(new Dictionary<string, string> { { "a", "=" } })); 
+        }
     }
 }
