7f4baaeae5a8a1a6c45315fc3e81d5530569fe60
Ralf Westphal <info@ralfw.de>
2012-12-10 12:20:37 +0100

    Red: Game still on

---
+++ Examples/CSharp/Tennis/Tennis/tests/test_Winner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tennis.tests
+{
+    [TestFixture]
+    public class test_Winner
+    {
+        [Test]
+        public void Game_still_on()
+        {
+            Assert.AreEqual("", winner);
+        }
+    }
+}
