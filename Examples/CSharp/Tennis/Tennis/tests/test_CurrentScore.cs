9ac407559d8667c6736f623859b5288bee364e2e
Ralf Westphal <info@ralfw.de>
2012-12-10 12:01:56 +0100

    Green: CurrentScore - No wins yet

--- Examples/CSharp/Tennis/Tennis/tests/test_CurrentScore.cs
+++ Examples/CSharp/Tennis/Tennis/tests/test_CurrentScore.cs
@@ -12,7 +12,7 @@ namespace Tennis.tests
         [Test]
         public void No_wins_yet()
         {
-            var POINT_VALUES = new string[] { "0", "15", "30", "40", "Advantage" };
+            var POINT_VALUES = new string[] { "Love", "15", "30", "40", "Advantage" };
             var score = string.Format("{0}:{1}", POINT_VALUES[0], POINT_VALUES[0]);
             Assert.AreEqual("Love:Love", score);
         }
