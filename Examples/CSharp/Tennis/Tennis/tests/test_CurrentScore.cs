784c2cc8aa3ba603c6f38fcecf63c0a11f116826
Ralf Westphal <info@ralfw.de>
2012-12-10 12:04:51 +0100

    Score after some wins
    Is green right away since Build_score() is internal and does the job

--- Examples/CSharp/Tennis/Tennis/tests/test_CurrentScore.cs
+++ Examples/CSharp/Tennis/Tennis/tests/test_CurrentScore.cs
@@ -16,5 +16,13 @@ namespace Tennis.tests
             var score = string.Format("{0}:{1}", POINT_VALUES[0], POINT_VALUES[0]);
             Assert.AreEqual("Love:Love", score);
         }
+
+        [Test]
+        public void Score_after_some_wins()
+        {
+            var sut = new Referee("A", "B", new[] {3, 2});
+            var score = sut.Build_score();
+            Assert.AreEqual("40:30", score);
+        }
     }
 }
