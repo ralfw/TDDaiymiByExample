f48807698f3074732a10163a1efeaef0ed3ef31c
Ralf Westphal <info@ralfw.de>
2012-12-12 16:11:19 +0100

    No winner while game is still on

---
+++ Examples/CSharp/Tennis/TennisV2/tests/test_Winner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TennisV2.tests
+{
+    [TestFixture]
+    public class test_Winner
+    {
+        [Test]
+        public void Game_still_on()
+        {
+            var sut = new Referee("A", "B");
+            var winner = "";
+            if (sut.Is_game_over())
+                winner = "And the winner is...";
+            Assert.AreEqual("", winner);
+        }
+    }
+}
