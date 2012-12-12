78fc0e4e833b9143a6dbd56c6e61b833f46f78dd
Ralf Westphal <info@ralfw.de>
2012-12-12 16:13:16 +0100

    Red: Winner when game over

--- Examples/CSharp/Tennis/TennisV2/tests/test_Winner.cs
+++ Examples/CSharp/Tennis/TennisV2/tests/test_Winner.cs
@@ -18,5 +18,15 @@ namespace TennisV2.tests
                 winner = "And the winner is...";
             Assert.AreEqual("", winner);
         }
+
+        [Test]
+        public void Game_is_over()
+        {
+            var sut = new Referee(new[] {"A", "B"}, new[] {4, 2});
+            var winner = "";
+            if (sut.Is_game_over())
+                winner = "And the winner is...";
+            Assert.AreEqual("A", winner);
+        }
     }
 }
