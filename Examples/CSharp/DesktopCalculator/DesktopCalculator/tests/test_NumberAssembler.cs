125ab0e961c7d6b9cef6e15485b6b783e6d9a204
Ralf Westphal <info@ralfw.de>
2012-12-14 22:05:37 +0100

    First digit is returned as number

---
+++ Examples/CSharp/DesktopCalculator/DesktopCalculator/tests/test_NumberAssembler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DesktopCalculator.tests
+{
+    [TestFixture]
+    public class test_NumberAssembler
+    {
+        [Test]
+        public void First_digit_is_returned_as_number()
+        {
+            const string digit = "2";
+            var result = int.Parse(digit);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
