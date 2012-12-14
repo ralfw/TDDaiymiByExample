d82e5d1f0e8e575c9b0532561950da517495aa52
Ralf Westphal <info@ralfw.de>
2012-12-14 21:58:38 +0100

    Adapted user interface test

--- Examples/CSharp/DesktopCalculator/DesktopCalculator/tests/test_UI.cs
+++ Examples/CSharp/DesktopCalculator/DesktopCalculator/tests/test_UI.cs
@@ -6,32 +6,47 @@ using System.Windows.Forms;
 using DesktopCalculator.domain;
 using DesktopCalculator.ui;
 using NUnit.Framework;
+using Application = System.Windows.Forms.Application;
 
 namespace DesktopCalculator.tests
 {
-    //[TestFixture]
-    //public class test_UI
-    //{
-    //    [Test, Explicit]
-    //    public void Run()
-    //    {
-    //        var calc = new MockCalculator();
-    //        var sut = new UI(calc);
+    [TestFixture]
+    public class test_UI
+    {
+        [Test, Explicit]
+        public void Run()
+        {
+            var app = new MockApplication();
+            var sut = new UI(app);
 
-    //        Application.Run(sut);
-    //    }
-    //}
+            Application.Run(sut);
+        }
+    }
 
-    //class MockCalculator : ICalculator
-    //{
-    //    public int Calculate(int number, string op)
-    //    {
-    //        Console.WriteLine("Calculate({0}, {1})", number, op);
+    class MockApplication : IApplication
+    {
+        public int Calculate(int number, string op)
+        {
+            Console.WriteLine("Calculate({0}, {1})", number, op);
 
-    //        if (number == 0) throw new DivideByZeroException();
+            if (number == 0) throw new DivideByZeroException();
 
-    //        return DateTime.Now.Second;
-    //    }
-    //}
+            return DateTime.Now.Second;
+        }
+
+        public int Assemble_number(string digit)
+        {
+            return int.Parse(digit)*10;
+        }
+
+        public int Calculate(string op)
+        {
+            Console.WriteLine("Calculate({0})", op);
+
+            if (op == "/") throw new DivideByZeroException();
+
+            return DateTime.Now.Second;
+        }
+    }
 
 }
